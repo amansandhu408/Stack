11c28644261096d75f579108149ac418d2f29cc2
amansandhu408 <amansandhu2241@gmail.com>
2025-07-20 02:34:34 +0530

    practice

--- TypeScript/dist/src/index.js
+++ TypeScript/dist/src/index.js
@@ -25,7 +25,38 @@ class Youtube {
         console.log("story");
     }
 }
-
-
 // abstract class : in abstract class , no new object can be created from this class
 // its the responsibiltty of extended class to create object from this class.
+// Generics
+const score = [];
+const names = [];
+function identityOne(val) {
+    return val;
+}
+// this is generics
+function identityTwo(val) {
+    return val; // if i give number , it returns number, and so on...
+}
+function getSearchProducts(products) {
+    // do some database operations
+    const myIndex = 3;
+    return products[myIndex];
+}
+// in arrow functions
+const getMorwSearchProducts = (product) => {
+    // do some database operations
+    const myIndex1 = 3;
+    return product[myIndex1];
+};
+// Narrowing
+function provideId(id) {
+    if (!id) {
+        console.log("please provide ID");
+        return;
+    }
+}
+function isAdminAccount(account) {
+    if ("isAdmin" in account) {
+        return account.isAdmin;
+    }
+}
--- TypeScript/src/index.ts
+++ TypeScript/src/index.ts
@@ -71,3 +71,61 @@ class Youtube implements TakePhoto , Story{
     }
 }
 
+// abstract class : in abstract class , no new object can be created from this class
+// its the responsibiltty of extended class to create object from this class.
+
+// Generics
+
+const score: Array<number> =[]
+const names: Array<string> =[]
+
+function identityOne(val:any ): any{
+    return val
+}
+                                                           // this is generics
+function identityTwo<Type>(val:Type):Type{                 // if i give string , it returns string
+    return val                                             // if i give number , it returns number, and so on...
+}
+
+function getSearchProducts<T>(products: T[]):T{
+    // do some database operations
+    const myIndex = 3;
+    return products[myIndex]
+}
+
+
+// in arrow functions
+
+const getMorwSearchProducts =<T,>(product:T[]): T=>{    //in github , developoers intentionally put , in <T ,> to let everyone know that its Generic 
+    // do some database operations
+    const myIndex1 = 3
+    return product[myIndex1]
+}
+
+
+// Narrowing
+
+function provideId(id :string | null ){
+    if (!id){
+        console.log("please provide ID")
+        return
+    }
+
+}
+
+interface User{
+    username: string
+    useremail: string
+}
+
+interface Admin{
+    name: string
+    email: string
+    isAdmin: boolean
+}
+
+function isAdminAccount(account: User | Admin){
+    if ("isAdmin" in account){
+        return account.isAdmin
+    }
+}

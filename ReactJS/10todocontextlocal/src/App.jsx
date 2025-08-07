4c6052842e24815c2fc4f4c4d99156966978aa90
Aman Preet Singh <131442723+amansandhu408@users.noreply.github.com>
2025-08-07 18:36:30 +0000

    practice

--- ReactJS/10todocontextlocal/src/App.jsx
+++ ReactJS/10todocontextlocal/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import {TodoProvider} from './contexts'
 import './App.css'
-//import TodoForm from './components/TodoForm'
-//import TodoItem from './components/TodoItem'
+import TodoForm from './components/TodoForm'
+import TodoItem from './components/TodoItem'
 
 function App() {
   const [todos, setTodos] = useState([])
